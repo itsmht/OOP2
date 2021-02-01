a054a2a8ebe819e0308b08e2e217222593c3e015
Mehedi Hasan Talha <50614875+itsmht@users.noreply.github.com>
2021-02-01 09:57:15 +0600

    Basic Operations

---
+++ Basic_CodeSln/Basic_Code/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 50, y = 2;
+            int add = x + y;
+            int sub = x - y;
+            int mult = x * y;
+            int div = x / y;
+            Console.WriteLine("Addition is " +add);
+            Console.WriteLine("Substraction is " + sub);
+            Console.WriteLine("Multiplication is " + mult);
+            Console.WriteLine("Division is " + div);
+            string a = Console.ReadLine();
+            int R = Int32.Parse(a);
+            Console.WriteLine(R);
+        }
+    }
+}
---
+++ CalcSln/Calc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int add = a + b;
+            int sub=a-b;
+            int mult= a*b;
+            int div= a/b;
+
+            
+        }
+    }
+}
