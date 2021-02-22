f9bffe84776d0d78824bc62410eb87b2e6f43744
Mehedi Hasan Talha <50614875+itsmht@users.noreply.github.com>
2021-02-22 10:56:21 +0600

    Inheritance Bank Account

---
+++ Array_Practice/Array_Practice/Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Practice
+{
+    class Array
+    {
+        
+    static void  Print2dArray(string[,] arr){
+    int r = arr.GetLength(0);
+        int c = arr.GetLength(1);
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            Console.Write(arr[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+    }
+}
---
+++ Array_Practice/Array_Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Practice{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            foreach (int n in arr)
+            {
+                Console.Write(n + " ");
+            }
+            string[,] arr2 = {
+                                 {"Karim","Mike"},
+                                 {"Sabrina","Tom"},
+                                 {"Poland","Husky"},
+                             };
+            
+
+        }
+    }
+}
